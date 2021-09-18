162ea877a4de28e1dd559ffeb90fec64bad82f81
Martin Muzatko <martin@happy-css.com>
2021-09-18 19:47:24 +0200

    added cache for uuid info

--- server/src/player/readPlayers.ts
+++ server/src/player/readPlayers.ts
@@ -2,13 +2,31 @@ import type { Dependencies } from '../dependencies'
 
 const minecraftApiUrl = 'https://sessionserver.mojang.com/session/minecraft/profile'
 const datFileEnding = '.dat'
+interface PlayerInfo {
+    id: string
+    name: string
+    properties: {
+        name: string
+        value: string
+    }[]
+}
+
+const uuidCache: Record<string, PlayerInfo> = {}
+
+const getPlayerInfoByUuid = async (dependencies: Dependencies, uuid: string) => {
+    if (uuid in uuidCache) return uuidCache[uuid]
+    uuidCache[uuid] = (await dependencies.lib.axios.get(`${minecraftApiUrl}/${uuid}`)).data
+    return uuidCache[uuid]
+}
+
 export const listPlayers = async (dependencies: Dependencies) => {
     const files = await dependencies.lib.fs.readdir(dependencies.config.player.datFilesFolderPath)
     return await Promise.all(files
         .filter(f => !f.endsWith('_old'))
         .map(async file => ({
             file,
-            info: (await dependencies.lib.axios.get(`${minecraftApiUrl}/${file.replace(datFileEnding, '')}`)).data
+            uuid: file.replace(datFileEnding, ''),
+            info: getPlayerInfoByUuid(dependencies, file.replace(datFileEnding, ''))
         }))
     )
 }
