313609496256843398511f792a44bfbe2e06579a
Martin Muzatko <martin@happy-css.com>
2021-09-19 19:43:17 +0200

    fix size for lootchests

--- client/pages/lootchests/index.vue
+++ client/pages/lootchests/index.vue
@@ -2,7 +2,7 @@
 	<div class="p-4 bg-gray-100 h-full w-full flex justify-center overflow-hidden">
 		<div class="xl:w-1/2 w-full shadow bg-white p-4">
 			<h2 class="mb-4 text-xl">Lootchests</h2>
-			<div class="xl:w-1/2 w-full">
+			<div>
 				<label class="ml-4 font-bold flex items-center">
 					Suche nach Lootchests
 					<div class="flex items-center bg-white shadow rounded overflow-hidden ml-2">
@@ -11,7 +11,7 @@
 					</div>
 				</label>
 				<div class="flex flex-wrap">
-					<div v-for="lootchest in sortedLootchests" :key="lootchest.id" class="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/5 xl:w-1/3">
+					<div v-for="lootchest in sortedLootchests" :key="lootchest.id" class="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/5 xl:w-1/3 2xl:w-1/4">
 						<nuxt-link :to="`/lootchests/${lootchest.id}`" class="block shadow rounded p-4 w-full h-full cursor-pointer bg-blue-800 text-white hover:bg-blue-600 transition">
 							{{ lootchest.id }}<br>
 							<div class="text-xs text-gray-400 font-sm">
